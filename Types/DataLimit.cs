1c771932fb8821585120b58b481dff6c4851438d
Lance Minecraft <98957643+LanceMinecraftFBA@users.noreply.github.com>
2023-03-05 19:39:48 +0300

    fixed file after uploading

--- Types/DataLimit.cs
+++ Types/DataLimit.cs
@@ -4,10 +4,10 @@ namespace OutlineManager.Types
 {
 	public class DataLimit
 	{
-        /// <value>Key ID</value>
-        public int KeyId;
-        /// <value>Set limit for key</value>
-        public long LimitBytes;
+		/// <value>Key ID</value>
+		public int KeyId;
+		/// <value>Set limit for key</value>
+		public long LimitBytes;
 	}
 }
 
