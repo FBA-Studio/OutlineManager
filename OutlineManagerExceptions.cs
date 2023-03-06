47d36f9c5f1c5c014d577b88055a3016f3bcc320
Lance Minecraft <98957643+LanceMinecraftFBA@users.noreply.github.com>
2023-03-06 14:45:52 +0300

    Add files via upload

---
+++ OutlineManagerExceptions.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace OutlineManagerExceptions
+{
+    [Serializable]
+    public class OutlineManagerException : Exception
+	{
+        public OutlineManagerException()
+        { }
+        public OutlineManagerException(string message)
+			: base(message)
+		{ }
+        public OutlineManagerException(string message, Exception innerException)
+        : base(message, innerException)
+        { }
+    }
+
+    [Serializable]
+	public class OutlineAPIException : Exception
+	{
+        public virtual int ErrorCode { get; }
+
+        public OutlineAPIException()
+        { }
+        public OutlineAPIException(string message)
+            : base(message)
+        { }
+        public OutlineAPIException(string message, Exception innerException)
+        : base(message, innerException)
+        { }
+    }
+}
+
