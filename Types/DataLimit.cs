5711f419c78f5ae0963aa07ee5841dc826ce109d
Lance Minecraft <98957643+LanceMinecraftFBA@users.noreply.github.com>
2023-03-06 14:46:47 +0300

    Fix Structure for JSON parse

--- Types/DataLimit.cs
+++ Types/DataLimit.cs
@@ -1,13 +1,11 @@
-﻿using System;
+using System;
 
 namespace OutlineManager.Types
 {
 	public class DataLimit
 	{
-		/// <value>Key ID</value>
-		public int KeyId;
 		/// <value>Set limit for key</value>
-		public long LimitBytes;
+		public long Bytes;
 	}
 }
 
